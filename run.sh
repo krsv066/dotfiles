39603ad92b5a1819eff691ca0ff7284f05e28d17
Kirill Agafonov <kaagafonov@edu.hse.ru>
2024-12-13 02:54:35 +0300

    init

--- run.sh
+++ run.sh
@@ -11,22 +11,22 @@ esac
 echo Installing configs for $machine
 
 if [ ${machine} == "Mac" ]; then
-    brew install tmux
-    brew install neovim
-    brew install stow
+    # brew install tmux
+    # brew install neovim
+    # brew install stow
 
-    brew install --cast alacritty
+    # brew install --cast alacritty
 
     # unnecessary
     # brew tap homebrew/cask
 
     # install font from here: https://www.nerdfonts.com/font-downloads
-    brew install --cask font-hack-nerd-font
+    # brew install --cask font-iosevka-term-nerd-font
 
 
     # show cmd+tab list on every connected display
     # https://www.reddit.com/r/mac/comments/pmoa2t/a_little_tip_for_people_using_multiple_displays/
-    defaults write com.apple.Dock appswitcher-all-displays -bool true; killall Dock
+    # defaults write com.apple.Dock appswitcher-all-displays -bool true; killall Dock
     # to revert:
     # defaults delete com.apple.Dock appswitcher-all-displays; killall Dock
 
@@ -39,6 +39,8 @@ elif [ ${machine} == "Linux" ]; then
     sudo apt -y install git wget curl
 
     sudo apt -y install clang-format
+
+    sudo apt -y install eza
 
     # install neovim
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -56,26 +58,26 @@ elif [ ${machine} == "Linux" ]; then
     yes N | ./ohmyzshsetup
 
     # plugin manager for tmux
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     # install plugins
-    ~/.tmux/plugins/tpm/bin/install_plugins
+    # ~/.tmux/plugins/tpm/bin/install_plugins
 fi
 
 
 
 # install vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install vim plug for nvim
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create symlinks for all files in this folder
 stow --adopt .
 
 #install plugins for vim and nvim
-vim +PlugInstall +qall > /dev/null
-nvim --headless +PlugInstall +qall > /dev/null
+# vim +PlugInstall +qall > /dev/null
+# nvim --headless +PlugInstall +qall > /dev/null
 
 chsh -s $(which zsh)
