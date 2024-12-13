3f6117191a35588e5ae514d8daa443ba798c55ca
Kirill Agafonov <kaagafonov@edu.hse.ru>
2024-12-13 16:03:15 +0300

    add reboot & fix

--- run.sh
+++ run.sh
@@ -66,10 +66,9 @@ if [ ${machine} == "Linux" ]; then
     #install zsh plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    zsh
     omz reload
-    path+=('/opt/nvim-linux64/bin')
-fi
+    echo export PATH=/opt/nvim-linux64/bin:$PATH >> ~/.zshrc
+    chsh -s $(which zsh)
 
-#set zsh as deefault shell
-chsh -s $(which zsh)
+    sudo reboot
+fi
